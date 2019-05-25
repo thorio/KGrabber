7f96b8426568de6bcc9b813fca8d26ac09ef10f2
thorio <thorio.git@gmail.com>
2019-05-25 16:55:27 +0200

    added json, csv and list exporters

--- KGrabber.user.js
+++ KGrabber.user.js
@@ -290,14 +290,43 @@ KG.exporters = {};
 KG.exporters.json = {
 	name: "json",
 	export: (data) => {
-		return "json? never heard of that";
+		var listing = $(".listing a").get().reverse();
+		var json = {
+			title: data.title,
+			server: data.server,
+			episodes: []
+		};
+		for (var i in data.episodes) {
+			json.episodes.push({
+				number: data.episodes[i].num,
+				name: listing[data.episodes[i].num-1].innerText,
+				link: data.episodes[i].grabLink
+			});
+		}
+		return JSON.stringify(json);
 	},
 }
 
 KG.exporters.csv = {
 	name: "csv",
 	export: (data) => {
-		return "wtf is csv";
+		var listing = $(".listing a").get().reverse();
+		var str = "episode, name, url\n";
+		for (var i in data.episodes) {
+			str += `${data.episodes[i].num}, ${listing[data.episodes[i].num-1].innerText}, ${data.episodes[i].grabLink}\n`;
+		}
+		return str;
+	}
+}
+
+KG.exporters.list = {
+	name: "list",
+	export: (data) => {
+		var str = "";
+		for (var i in data.episodes) {
+			str += data.episodes[i].grabLink + "\n";
+		}
+		return str;
 	}
 }
 

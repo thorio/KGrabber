fd48f0428ad3666179e6f2d1bf7d8a4fe1862946
thorio <thorio.git@gmail.com>
2019-10-26 16:32:43 +0200

    Improve logging, ajax helpers and json parsing

--- src/js/util.js
+++ src/js/util.js
@@ -40,57 +40,66 @@ KG.timeout = (time) => {
 	});
 }
 
+//returns parsed json or undefined if parse fails
+KG.tryParseJson = (str) => {
+	try {
+		return JSON.parse(str);
+	} catch {}
+}
 //#endregion
 
 //#region Logging
 var logCss = "background-color: #456304; padding: 0 5px; border-radius: 3px; color: #fff;";
 
-KG.loginfo = (...str) => {
-	console.info("%cKissGrabber%c " + str.join(" "), logCss, "");
+KG.loginfo = (...obj) => {
+	console.info("%cKissGrabber", logCss, ...obj);
 }
 
-KG.log = (...str) => {
-	console.log("%cKissGrabber%c " + str.join(" "), logCss, "");
+KG.log = (...obj) => {
+	console.log("%cKissGrabber", logCss, ...obj);
 }
 
-KG.logwarn = (...str) => {
-	console.warn("%cKissGrabber%c " + str.join(" "), logCss, "");
+KG.logwarn = (...obj) => {
+	console.warn("%cKissGrabber", logCss, ...obj);
 }
 
-KG.logerr = (...str) => {
-	console.error("%cKissGrabber%c " + str.join(" "), logCss, "");
+KG.logerr = (...obj) => {
+	console.error("%cKissGrabber", logCss, ...obj);
 }
 //#endregion
 
 //#region Ajax Helpers
-KG.get = (url) => {
+KG.get = (url, headers) => {
 	return new Promise((resolve, reject) => {
 		GM_xmlhttpRequest({
 			method: "GET",
-			url: url,
+			url,
+			headers,
 			onload: resolve,
 			onerror: reject,
 		});
 	});
 }
 
-KG.head = (url) => {
+KG.head = (url, headers) => {
 	return new Promise((resolve, reject) => {
 		GM_xmlhttpRequest({
 			method: "HEAD",
-			url: url,
+			url,
+			headers,
 			onload: resolve,
 			onerror: reject,
 		});
 	});
 }
 
-KG.post = (url, body) => {
+KG.post = (url, data, headers) => {
 	return new Promise((resolve, reject) => {
 		GM_xmlhttpRequest({
 			method: "POST",
-			url: url,
-			body: body,
+			url,
+			data,
+			headers,
 			onload: resolve,
 			onerror: reject,
 		});

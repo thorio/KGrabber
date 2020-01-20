640dc75dd159cd7cbdd8fcd0072f4978f879e6ec
thorio <thorio.git@gmail.com>
2020-01-20 22:23:01 +0100

    Repackage logging into Logger class

--- src/js/util/log.js
+++ src/js/util/log.js
@@ -1,22 +1,6 @@
-let logCss = "background-color: #456304; padding: 0 5px; border-radius: 3px; color: #fff;";
-let prefix = "KGrabber";
+const { Logger } = require("kgrabber-types");
 
-exports.info = (...obj) => {
-	console.info(`%c${prefix}`, logCss, ...obj);
-};
-
-exports.log = (...obj) => {
-	console.log(`%c${prefix}`, logCss, ...obj);
-};
-
-exports.warn = (...obj) => {
-	console.warn(`%c${prefix}`, logCss, ...obj);
-};
-
-exports.err = (...obj) => {
-	console.error(`%c${prefix}`, logCss, ...obj);
-};
-
-exports.debug = (...obj) => {
-	console.debug(`%c${prefix}`, logCss, ...obj);
-};
+module.exports = new Logger("KGrabber", {
+	color: "#eee",
+	backgroundColor: "#456304",
+});
